5b0d5b87d2635612751c1096d848d4a8344e6d83
Luuiis14 <luis.henriquez52@inacapmail.cl>
2024-12-18 21:26:52 -0300

    Update api.py

--- EVALUACION-3-FINAL/EVALUACION-3-main-main/EVALUACION-3-main/evaluacion 3/servicios/api.py
+++ EVALUACION-3-FINAL/EVALUACION-3-main-main/EVALUACION-3-main/evaluacion 3/servicios/api.py
@@ -1,11 +1,42 @@
-# servicios/api.py
 import requests
 from auxiliares.constantes import URL_API
 
 def obtener_datos(tipo):
-    response = requests.get(f"{URL_API}/{tipo}")
-    return response.json()
+    try:
+        response = requests.get(f"{URL_API}/{tipo}")
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
 
 def enviar_datos(tipo, datos):
-    response = requests.post(f"{URL_API}/{tipo}", json=datos)
-    return response.status_code
+    try:
+        response = requests.post(f"{URL_API}/{tipo}", json=datos)
+        response.raise_for_status()
+        return response.status_code
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+
+def actualizar_datos(tipo, datos):
+    try:
+        response = requests.put(f"{URL_API}/{tipo}", json=datos)
+        response.raise_for_status()
+        return response.status_code
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+
+def eliminar_datos(tipo, id_dato):
+    try:
+        response = requests.delete(f"{URL_API}/{tipo}/{id_dato}")
+        response.raise_for_status()
+        return response.status_code
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
